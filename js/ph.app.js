19d7d9cf387c39e612735a2a4658df7e506b278f
usuario <usuario@pc-01>
2016-11-21 09:17:48 -0430

    Nuevos cambias - Arreglos en HTML y CSS - Unificación del Menú

--- js/ph.app.js
+++ js/ph.app.js
@@ -1,5 +1,21 @@
-(function(){
+'use strict';
 
-	angular
-		.module("phApp", []);
-})();
+var app = angular.module('pizza', ['ngRoute', 'ngResource']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'js/pizza/pizza.html',
+			controller: 'PizzaCtrl'
+		})
+		.when('/purchase', {
+			templateUrl: 'js/purchase/purchase.html',
+			controller: 'PurchaseCtrl'
+		})
+		.when('/about-me', {
+			templateUrl: 'js/nav/about-me.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
