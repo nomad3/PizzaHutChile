772a44542c739f1a15c2c2a31a60b8dd2aad0a69
usuario <usuario@pc-01>
2016-11-22 09:37:23 -0430

    Corrección de bug en menú

--- js/ph.app.js
+++ js/ph.app.js
@@ -1,21 +1,5 @@
-'use strict';
+(function(){
 
-var app = angular.module('pizza', ['ngRoute', 'ngResource']);
-
-app.config(function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'js/pizza/pizza.html',
-			controller: 'PizzaCtrl'
-		})
-		.when('/purchase', {
-			templateUrl: 'js/purchase/purchase.html',
-			controller: 'PurchaseCtrl'
-		})
-		.when('/about-me', {
-			templateUrl: 'js/nav/about-me.html'
-		})
-		.otherwise({
-			redirectTo: '/'
-		});
-});
+	angular
+		.module("phApp", []);
+})();
